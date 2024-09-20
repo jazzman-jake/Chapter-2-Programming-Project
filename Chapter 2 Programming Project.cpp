8ee062c0165e6b8d9c789a0a2da6adddb23c12a9
Jake Williamson <williamsonj0403@gmail.com>
2024-09-20 03:10:47 -0400

    set variables for number of energy drink customers and number of those customers who prefer citrus drinks. printed data to screen to show calculations

--- Chapter 2 Programming Project.cpp
+++ Chapter 2 Programming Project.cpp
@@ -1,20 +1,28 @@
-// Chapter 2 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Terrence Williamson
+//09.19.24
+//Chapter 2 Programming Project
+//Energy-Drink
+
+//total customers starting 16,500
+//15% bought energy drink per week
+//58% of the 15% prefer citrus
 
 #include <iostream>
+using namespace std;
+//intializing variables for total customers surveyed, percentage of customers-
+//-who buy energy drinks every week and who, of that group, prefer citrus
+int surv = 16'500;
+//0.15: 15% of total surv bought energy drinks
+float drinkers = 0.15 * surv;
+//0.58: 58% of buyers prefer citrus flavored drinks
+int citrus = 0.58 * drinkers;
 
 int main()
-{
-    std::cout << "Hello World!\n";
+{//print data to screen
+    cout << "A soft drink company surveyed 16,500 of its customers.\n";
+    cout << drinkers;
+    cout << " of those surveyed buy energy drinks once or more weekly.\n";
+    cout << citrus;
+    cout << " of those who buy the drinks prefer citrus flavored versions.\n\n";  
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
